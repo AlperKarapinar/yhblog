d419e44cffee2028eabb48171d52b1115f529fdd
kader <kadersk@gmail.com>
2011-05-18 13:16:42 +0300

    fixes in user crud

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :first_name, :last_name, :author, :admin, :active
   
   validates_presence_of :email, :first_name, :last_name
   validates_uniqueness_of :email
