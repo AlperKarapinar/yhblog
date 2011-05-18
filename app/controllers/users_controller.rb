ad54ef0e333274032df844d2eb32e6cb37c2d751
kader <kadersk@gmail.com>
2011-05-18 14:22:06 +0300

    sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  
+  
   # GET /users
   # GET /users.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,25 @@
 Yhblog::Application.routes.draw do
+  get "sessions/new"
+
+  get "sessions/create"
+
+  get "sessions/destroy"
+
+  get "new/create"
+
+  get "new/destroy"
+  
+  get "log_in" => "sessions#new", :as => "log_in"
+  
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  
+  get "sign_up" => "users#new", :as => "sign_up" 
+
   resources :users
 
   resources :posts
+  
+  resources :sessions
 
   root :to => "posts#index"
 end
